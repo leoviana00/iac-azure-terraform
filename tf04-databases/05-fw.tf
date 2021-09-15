aec6897b3f340e89ee1125c8caa5a367036067d3
Leonardo Viana <leonardoviana00@hotmail.com>
2021-09-14 22:24:30 -0300

    add regras para pgsql

--- tf04-databases/05-fw.tf
+++ tf04-databases/05-fw.tf
@@ -4,7 +4,7 @@ data "http" "ip_address" {
         Accept = "text/plan"
     }
 }
-
+# Regras SQL SERVER
 resource "azurerm_sql_firewall_rule" "sqlfirewall_myip" {
     name = "personal-ip"
     resource_group_name = azurerm_resource_group.rg.name
@@ -17,6 +17,23 @@ resource "azurerm_sql_firewall_rule" "sqlfirewall_azureservices" {
     name = "allow-azure-services-pulse"
     resource_group_name = azurerm_resource_group.rg.name
     server_name = azurerm_sql_server.sqlserver.name
+    start_ip_address = "0.0.0.0"
+    end_ip_address = "0.0.0.0"
+}
+
+#Regras POSTGRESQL
+resource "azurerm_postgresql_firewall_rule" "pgsqlfirewall_myip" {
+    name = "personal-ip"
+    resource_group_name = azurerm_resource_group.rg.name
+    server_name = azurerm_postgresql_server.pgsql.name
+    start_ip_address = data.http.ip_address.body
+    end_ip_address = data.http.ip_address.body
+}
+
+resource "azurerm_postgresql_firewall_rule" "pgsqlfirewall_azureservices" {
+    name = "allow-azure-services-pulse"
+    resource_group_name = azurerm_resource_group.rg.name
+    server_name = azurerm_postgresql_server.pgsql.name
     start_ip_address = "0.0.0.0"
     end_ip_address = "0.0.0.0"
 }
